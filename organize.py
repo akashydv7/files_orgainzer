2447968950bb22eddbab8511fba406915a6baf34
Akash <56221005+akashydv7@users.noreply.github.com>
2021-07-24 15:03:14 +0530

    Update organize.py
    more directories and file types added

--- organize.py
+++ organize.py
@@ -1,46 +1,51 @@
 import os, shutil
 
-#change the path to desired directory
+# CHANGE THE PATH TO THE DESIRED DIRECTORY
 path = r"C:\Users\username\Downloads"
 
-files = [f for f in os.listdir(path)]
+# LISTING ALL FILES IN DIRECTORY
+files = [f for f in os.listdir(path) if os.path.isfile(path+rf"\{f}")]
 
-folders = ["applications", "music", "zip files", "images", "docs", "videos", "presentations"]
+# LIST OF ALL FOLDERS FOR STORING FILES
+folders = ["applications", "audio", "zip files", "images", "docs", "videos", "presentations", "webpages", "codes"]
 
+# EXTENSIONS POINTING TO RESPECTIVE DIRECTORY
+directories = {
+
+    ".html5.html.htm.xhtml.aspx.php.webp": "webpages",
+
+    ".jpg.jpeg.png.tiff.gif.bmp.bpg.svg.heif.psd.jfif": "images" ,
+
+    ".avi.mp4.flv.mkv.wmv.mov.webm.vob.3gp.mpeg.mpg.qt": "videos",
+
+    ".pdf.doc.docx.xls.xlsx.ppt.pptx.epub.opus.txt.in.out.xml": "docs",
+
+    ".rar.zip.7z": "zip files" ,
+
+    ".mp3.aac.ogg.m4a.wav.aa.dvf.m4b.m4p.msv.oga.raw.vox": "audio" ,
+
+     ".exe.apk.msi":"applications",
+
+     ".py.cpp.c++.cp.js.jsp.ipynb.java": "codes"
+
+}
+
+existing_dir = [d for d in os.listdir(path) if os.path.isdir(path+rf"\{d}")]
+# CREATING NEW DIRECTORIES IF NOT ALREADY PRESENT
 for dir in folders:
-    if dir in files:
+    if dir in existing_dir:
         continue
     os.mkdir(path + rf"\{dir}")
 
-apps = path + r"\applications"
-music = path + r"\music"
-presentations = path + r"\presentations"
-zips = path + r"\zip files"
-images = path + r"\images"
-docs = path + r"\docs"
-vids = path +r"\videos"
-
-#sorting files on the basis of their extension
-
+# MOVING FILES TO RESPECTIVE DIRECTORIES
 for x in files:
+     suffix = x.split(".")[-1]
      f_path = path + rf"\{x}"
-     if x.endswith(".mp3"):
-          shutil.move(f_path, music)
-
-     elif x.endswith(".exe"):
-          shutil.move(f_path, apps)
-
-     elif x.endswith(".pdf") or x.endswith(".docx") or x.endswith(".xlsx") or x.endswith("txt"):
-          shutil.move(f_path, docs)
-
-     elif x.endswith(".rar") or x.endswith(".zip"):
-          shutil.move(f_path, zips)
-
-     elif x.endswith(".pptx"):
-          shutil.move(f_path, presentations)
-
-     elif x.endswith(".png") or x.endswith(".jpg")or x.endswith(".jpeg"):
-          shutil.move(f_path, images)
-
-     elif x.endswith(".mp4") or x.endswith(".mkv"):
-          shutil.move(f_path, vids)
+     for key in directories.keys():
+          if suffix in key:
+               try:
+                    shutil.move(f_path, path+rf"\{directories[key]}")
+               # IN CASE OF ANY EXCEPTIONS IGNORE CURRENT FILE 
+               # (usually occurs when file is already present in the directory)
+               except Exception as e:
+                    print(f"Failed to move {x} : {e}")
